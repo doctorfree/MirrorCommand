c397f1b393af9780642978ddf0f1d867d3cc577f
Ronald Record <github@ronrecord.com>
2021-10-15 09:36:08 -0700

    Update mounted filesystem

--- reboot.sh
+++ reboot.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-for fs in Seagate_8TB WD_My_Passport_4TB
-do
-    mount | grep -v /etc/auto.mount | grep $fs > /dev/null && sudo umount /home/pi/$fs
-done
+
+sudo umount /mnt/transcend
+sleep 2
 sudo /sbin/shutdown -r now
