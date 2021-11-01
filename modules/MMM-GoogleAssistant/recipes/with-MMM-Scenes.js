92099aa4a328158fc1f6bc078eab14f4c6b34f4d
Ronald Record <github@ronrecord.com>
2021-11-01 16:29:23 -0700

    Split MMM-Scenes voice commands out into its own recipe

---
+++ modules/MMM-GoogleAssistant/recipes/with-MMM-Scenes.js
@@ -0,0 +1,99 @@
+/**   with-MMM-Scenes                                 **/
+/**   Voice commands script to manage MMM-Scenes      **/
+/**   Requires MirrorCommandLine:                     **/
+/**   https://gitlab.com/doctorfree/MirrorCommandLine **/
+/**   @doctorfree                                     **/
+
+var recipe = {
+  transcriptionHooks: {
+    "SCENES_NEXT": {
+      pattern: "next scene",
+      command: "SCENES_NEXT"
+    },
+    "SCENES_PREV": {
+      pattern: "previous scene",
+      command: "SCENES_PREV"
+    },
+    "SCENES_ACT_ONE": {
+      pattern: "scene one|scene 1",
+      command: "SCENES_ACT_ONE"
+    },
+    "SCENES_ACT_TWO": {
+      pattern: "scene two|scene 2",
+      command: "SCENES_ACT_TWO"
+    },
+    "SCENES_ACT_THREE": {
+      pattern: "scene three|scene 3",
+      command: "SCENES_ACT_THREE"
+    },
+    "SCENES_ACT_FOUR": {
+      pattern: "scene four|scene 4",
+      command: "SCENES_ACT_FOUR"
+    },
+    "SCENES_ACT_FIVE": {
+      pattern: "scene five|scene 5",
+      command: "SCENES_ACT_FIVE"
+    },
+  },
+
+  commands: {
+    "SCENES_NEXT": {
+      shellExec: {
+        exec: "mirror scene next"
+      },
+      soundExec: {
+        chime: "open"
+      }
+    },
+    "SCENES_PREV": {
+      shellExec: {
+        exec: "mirror scene prev"
+      },
+      soundExec: {
+        chime: "open"
+      }
+    },
+    "SCENES_ACT_ONE": {
+      shellExec: {
+        exec: "mirror scene 0"
+      },
+      soundExec: {
+        chime: "open"
+      }
+    },
+    "SCENES_ACT_TWO": {
+      shellExec: {
+        exec: "mirror scene 1"
+      },
+      soundExec: {
+        chime: "open"
+      }
+    },
+    "SCENES_ACT_THREE": {
+      shellExec: {
+        exec: "mirror scene 2"
+      },
+      soundExec: {
+        chime: "open"
+      }
+    },
+    "SCENES_ACT_FOUR": {
+      shellExec: {
+        exec: "mirror scene 3"
+      },
+      soundExec: {
+        chime: "open"
+      }
+    },
+    "SCENES_ACT_FIVE": {
+      shellExec: {
+        exec: "mirror scene 4"
+      },
+      soundExec: {
+        chime: "open"
+      }
+    },
+  }
+}
+
+exports.recipe = recipe // Don't remove this line.
