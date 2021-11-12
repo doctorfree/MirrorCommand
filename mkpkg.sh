f0a1f7d453b84ceaa9151b714a558eb06b2e3a40
Ronald Record <github@ronrecord.com>
2021-11-12 14:54:22 -0800

    Add Artists images package

--- mkpkg.sh
+++ mkpkg.sh
@@ -93,7 +93,6 @@ cd "${SRC}/${SRC_NAME}"
 
 PKG="mirror-images-portrait"
 PKG_NAME="MirrorImagesPortrait"
-SRC=${HOME}/src
 # Subdirectory in which to create the distribution files
 OUT_DIR="dist/${PKG_NAME}_${PKG_VER}"
 
@@ -134,7 +133,6 @@ cd "${SRC}/${SRC_NAME}"
 
 PKG="photographers-portrait"
 PKG_NAME="PhotographersPortrait"
-SRC=${HOME}/src
 # Subdirectory in which to create the distribution files
 OUT_DIR="dist/${PKG_NAME}_${PKG_VER}"
 
@@ -154,6 +152,46 @@ Standards-Version: 4.1.3
 Homepage: https://gitlab.com/doctorfree/MirrorCommandLine
 Description: MagicMirror Photographer Images
  Photographer Images for a MagicMirror using the MirrorCommandLine configs" > ${OUT_DIR}/DEBIAN/control
+
+for dir in "${TOP}" "${TOP}/share" "${TOP}/share/doc" "${TOP}/share/doc/${PKG}"
+do
+    [ -d ${OUT_DIR}/${dir} ] || sudo mkdir ${OUT_DIR}/${dir}
+    sudo chown root:root ${OUT_DIR}/${dir}
+done
+
+sudo cp AUTHORS ${OUT_DIR}/${TOP}/share/doc/${PKG}/AUTHORS
+sudo cp LICENSE ${OUT_DIR}/${TOP}/share/doc/${PKG}/copyright
+sudo cp CHANGELOG.md ${OUT_DIR}/${TOP}/share/doc/${PKG}/changelog
+sudo cp README.md ${OUT_DIR}/${TOP}/share/doc/${PKG}/README
+sudo gzip -9 ${OUT_DIR}/${TOP}/share/doc/${PKG}/changelog
+
+cd dist
+echo "Building ${PKG_NAME}_${PKG_VER} package"
+sudo dpkg-deb --build ${PKG_NAME}_${PKG_VER}
+
+cd "${SRC}/${SRC_NAME}"
+
+PKG="artists-portrait"
+PKG_NAME="ArtistsPortrait"
+# Subdirectory in which to create the distribution files
+OUT_DIR="dist/${PKG_NAME}_${PKG_VER}"
+
+[ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR}
+mkdir ${OUT_DIR}
+cp -a pkgart ${OUT_DIR}/DEBIAN
+
+echo "Package: ${PKG}
+Version: ${PKG_VER}
+Section: misc
+Priority: optional
+Architecture: armhf
+Depends: mirrorcommandline (>= 2.2)
+Maintainer: ${DEBFULLNAME} <${DEBEMAIL}>
+Build-Depends: debhelper (>= 11)
+Standards-Version: 4.1.3
+Homepage: https://gitlab.com/doctorfree/MirrorCommandLine
+Description: MagicMirror Artists Images
+ Artists Images for a MagicMirror using the MirrorCommandLine configs" > ${OUT_DIR}/DEBIAN/control
 
 for dir in "${TOP}" "${TOP}/share" "${TOP}/share/doc" "${TOP}/share/doc/${PKG}"
 do
