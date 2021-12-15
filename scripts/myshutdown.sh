cd1dc6bcd1bd6bcf9a429f10f94a3a5a73ba3650
Ronald Record <github@ronrecord.com>
2021-12-15 11:15:58 -0800

    Only unmount disks if they are mounted

--- scripts/myshutdown.sh
+++ scripts/myshutdown.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+#
+# List of mount points to unmount before rebooting
+MNT_PTS="/mnt/transcend"
 
 mirror_inst=`type -p mirror`
 [ "${mirror_inst}" ] && {
     mirror stop
 }
-sleep 2
-sudo umount /mnt/transcend
+
+for mntpt in ${MNT_PTS}
+do
+  mount | grep ${mntpt} > /dev/null && {
+    sudo umount ${mntpt}
+  }
+done
+
 sleep 2
 sudo /sbin/shutdown -h now
