0f7ac07ffcc07ddf3184b89286165aa80f391b58
Ronald Record <github@ronrecord.com>
2022-04-04 19:50:03 -0700

    Add configs for photographer Andrey Popov

---
+++ config-notelegram/Photographers/config-Andrey_Popov.js
@@ -0,0 +1,139 @@
+/* Magic Mirror Config
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * Modified by Ron Record http://ronrecord.com
+ * MIT Licensed.
+ *
+ * For more information how you can configurate this file
+ * See https://github.com/MichMich/MagicMirror#configuration
+ *
+ */
+
+var config = {
+//	address: "localhost",
+	address: "0.0.0.0", // Address to listen on, can be:
+	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	                      // - another specific IPv4/6 to listen on a specific interface
+	                      // - "", "0.0.0.0", "::" to listen on any interface
+	                      // Default, when address config is left out, is "localhost"
+	port: 8080,
+    // Set [] to allow all IP addresses
+	// or add a specific IPv4 of 192.168.1.5 :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+    ipWhitelist: [
+        "0.0.0.0",
+        "127.0.0.1",
+        "AA.A.A.AA", // Mac Mini
+        "BB.B.B.BBB", // iPad
+        "CC.C.C.CC", // Mac Pro
+        "DD.D.D.DDD", // Mac Pro over Tunnelblick
+        "EE.E.E.EE", // Roon Core
+        "FF.F.F.FF", // Mac Pro
+        "GG.G.G.GG", // Ropieee
+        "MM.M.M.MM", // Raspberry Pi MagicMirror
+        "HH.H.H.HH", // Raspberry Pi 400
+        "II.I.I.II", // iPad Air
+        "JJ.J.J.JJ", // iPhone 12 Mini
+        "::ffff:127.0.0.1",
+        "::1",
+    ],
+
+	customCss: "css/custom-mirrorcommand.css",
+    language: "en",
+	timeFormat: 12,
+	units: "imperial",
+	// serverOnly:  true/false/"local" ,
+			     // local for armv6l processors, default 
+			     //   starts serveronly and then starts chrome browser
+			     // false, default for all  NON-armv6l devices
+			     // true, force serveronly mode, because you want to.. no UI on this device
+	
+	electronOptions: {
+        x: 0, // __X_OFFSET__ Do Not Remove
+        y: 0, // __Y_OFFSET__ Do Not Remove
+    },
+
+    modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+        {
+            module: 'MMM-Remote-Control',
+            config: {
+                apiKey: 'xxx_Remote-Control-API-Key_xxxxx',
+                customCommand: {
+                    shutdownCommand: '/usr/local/bin/myshutdown',
+                    rebootCommand: '/usr/local/bin/myreboot',
+                    monitorOnCommand: 'vcgencmd display_power 1',
+                    monitorOffCommand: 'vcgencmd display_power 0',
+                    screenshotCommand: '/usr/local/bin/mirror screenshot',
+                    rotateScreenRight: '/usr/local/bin/mirror rotate right',
+                    rotateScreenLeft: '/usr/local/bin/mirror rotate left',
+                    rotateScreenNormal: '/usr/local/bin/mirror rotate normal',
+                    rotateScreenInverted: '/usr/local/bin/mirror rotate inverted',
+                    playVideo: '/usr/local/bin/mirror playvideo',
+                    pauseVideo: '/usr/local/bin/mirror pausevideo',
+                    replayVideo: '/usr/local/bin/mirror replayvideo',
+                    nextVideo: '/usr/local/bin/mirror nextvideo',
+                    hideVideo: '/usr/local/bin/mirror hidevideo',
+                    showVideo: '/usr/local/bin/mirror showvideo',
+                    // Shell command to return status of monitor,
+                    // must return either "HDMI" or "true" if screen is on
+                    // "TV is Off" or "false" if it is off to be recognized
+                    // monitorStatusCommand: '/usr/local/bin/mirror screen status',
+                },
+                showModuleApiMenu: true,
+                secureEndpoints: true,
+                customMenu: "custom_menu.json",
+                // classes: {} // Optional, See "Custom Classes" below
+            }
+        },
+        {
+            module: 'MMM-BackgroundSlideshow',
+            position: 'fullscreen_below',
+            // classes: 'scheduler',
+            config: {
+                imagePaths: [
+                    '/usr/local/MirrorCommand/pics/Photographers/Andrey_Popov',
+                ],
+                slideshowSpeed: 15000, // 15 seconds
+                resizeImages: true,
+                maxWidth: __WIDTH__, // __SET_MAX_WIDTH__ Do Not Remove
+                maxHeight: __HEIGHT__, // __SET_MAX_HEIGHT__ Do Not Remove
+                transitionImages: true,
+                randomizeImageOrder: true,
+                recursiveSubDirectories: true,
+                resizeImages: true,
+                maxWidth: __WIDTH__, // __SET_MAX_WIDTH__ Do Not Remove
+                maxHeight: __HEIGHT__, // __SET_MAX_HEIGHT__ Do Not Remove
+                transitions: ['opacity', 'slideFromRight', 'slideFromLeft', 'slideFromTopLeft', 'slideFromTopRight', 'slideFromBottomLeft', 'slideFromBottomRight', 'flipX', 'flipY'],
+                //backgroundSize: "contain",
+                // DISPLAY THE SLIDE SHOW BETWEEN 1PM and 6PM then again between 9PM and Midnight
+                //module_schedule: [
+                //  {from: '0 13 * * *', to:   '0 18 * * *'},
+                //  {from: '0 21 * * *', to:   '59 23 * * *'}
+                //]
+            }
+        },
+        // {
+        //     module: "MMM-GoogleAssistant",
+        //     position: "top_right",
+        //     config: {
+        //         maxWidth: "100%",
+        //         header: "",
+        //     publishKey: 'xxxxxx_Your-GoogleVoice-Pub-Key_xxxxxxxx',
+        //     subscribeKey: 'xxxxxx_Your-GoogleVoice-Sub-Key_xxxxxxxx',
+        //     updateDelay: 500
+        //     }
+        // },
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = config;}
